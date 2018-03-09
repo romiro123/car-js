23b9b00f000f0e116e5ab45d47a12af38d3a4a07
Roman Kilennikov <kilennikovroman@gmail.com>
2018-03-09 17:06:41 +0300

    work at the lesson

---
+++ car.js
@@ -0,0 +1,38 @@
+var sedan = document.getElementById('sedan');
+var getCar = document.getElementById('getCar');
+var position = 0;
+var id;
+//событие при наведение курсора
+getCar.onmouseover = function() {
+	//console.log('over');
+	
+	if(id) clearInterval(id);
+	id = setInterval(toDriveRight, 2);
+}
+
+//отвода курсора
+getCar.onmouseout = function(){
+	//console.log('out')
+	
+	clearInterval(id);
+	id = setInterval(toDriveLeft, 2);
+}
+
+function toDriveRight(){
+	if(position == 500){
+		clearInterval(id);
+	}else{
+		position ++;
+		sedan.style.left = position + 'px';
+	}
+}
+
+function toDriveLeft(){
+	if (position == 0){
+		clearInterval(id);
+		
+	}else{
+		position --;
+		sedan.style.left = position + 'px';
+	}
+}
